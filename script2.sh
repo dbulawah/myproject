470fce87b944b0db9d4dc80dbdc2f64bb26da6d8
dbulawah <109478994+dbulawah@users.noreply.github.com>
2025-02-15 10:35:21 -0600

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,21 +1,4 @@
-I have created a branch, it name is branch
-
-New line added
-
-
-Adding another change
-
-3rd commit changes
-s1
- rosina
-
-
-i am doing another updat
-
-
-I am demoing stage 
-
-i will make change
-
-
-adding mores
+date
+ls 
+pwd
+echo " am learning jenkins"
